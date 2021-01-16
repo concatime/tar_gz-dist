54da83111ab268d2b816c410969c4d53ebdc9f27
Issam E. Maghni <issam.e.maghni@mailbox.org>
2021-01-16 17:19:08 -0500

    script

--- CONVERT.sh
+++ CONVERT.sh
@@ -4,23 +4,23 @@ if test -f "$1"; then
 	set -- "${1%.*.*}" "$1" "${1##*.}"
 
 	case "$3" in
-		br)  PROG='brotli' ;;
-		bz2) PROG='bzip2' ;;
-		gz)  PROG='pigz' ;;
-		lz)  PROG='lzip' ;;
-		lz4) PROG='lz4' ;;
-		tgz) PROG='pigz' && set "${2%.*}" "$2" ;;
-		xz)  PROG='xz' ;;
-		zst) PROG='zstd' ;;
+		br)  CMD='brotli' ;;
+		bz2) CMD='bzip2' ;;
+		gz)  CMD='pigz' ;;
+		lz)  CMD='lzip' ;;
+		lz4) CMD='lz4' ;;
+		tgz) CMD='pigz' && set "${2%.*}" "$2" ;;
+		xz)  CMD='xz' ;;
+		zst) CMD='zstd' ;;
 		*)   >&2 echo "UNRECOGNIZED TYPE ${3}" && exit 1 ;;
 	esac
 
-	if ! >'/dev/null' command -v "$PROG"; then
-		>&2 echo "COMMAND ${PROG} NOT FOUND"
+	if ! >'/dev/null' command -v "$CMD"; then
+		>&2 echo "COMMAND ${CMD} NOT FOUND"
 		exit 1
 	fi
 
-	<"$2" "$PROG" -d | pax -r
+	<"$2" "$CMD" -d | pax -r
 	rm "$2"
 elif ! test -d "$1"; then
 	>&2 echo "ARGUMENT ${1} IS NEITHER A FILE NOR A DIRECTORY"
