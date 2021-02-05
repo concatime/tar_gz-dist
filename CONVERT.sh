28adeec180a2263f9a62334557fb68451e7ecf26
foo@bar.com <foo@bar.com>
2021-02-05 12:44:32 -0500

    + m4-1.4.18

--- CONVERT.sh
+++ CONVERT.sh
@@ -30,7 +30,7 @@ fi
 set -- "$1" "${1}.tar.gz"
 
 pax -w "$1" | >"$2" pigz -c -11
-rm -R "$1"
+rm -Rf "$1"
 
 test -f 'SHA256.txt' || touch 'SHA256.txt'
 <'SHA256.txt' >'SHA256.txt.new' sed "/${2}/d"
